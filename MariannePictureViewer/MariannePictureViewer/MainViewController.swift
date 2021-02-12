22921f607d0ec9236198238cbd33be44df87da77
romankolossov <rkolossov@gmail.com>
2021-02-12 21:31:40 +0300

    app created and scene configurated

---
+++ MariannePictureViewer/MariannePictureViewer/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  MariannePictureViewer
+//
+//  Created by Roman Kolosov on 12.02.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
