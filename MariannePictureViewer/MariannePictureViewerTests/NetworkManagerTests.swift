beb302f8b3218b02fd23886b4ab893fcecc34c6b
romankolossov <rkolossov@gmail.com>
2021-02-22 15:14:06 +0300

    NetworkManager tests complete

---
+++ MariannePictureViewer/MariannePictureViewerTests/NetworkManagerTests.swift
@@ -0,0 +1,83 @@
+//
+//  NetworkManagerTests.swift
+//  MariannePictureViewerTests
+//
+//  Created by Roman Kolosov on 22.02.2021.
+//
+
+import XCTest
+@testable import MariannePictureViewer
+
+class NetworkManagerTests: XCTestCase {
+    
+    // MARK: - Positive tests
+    
+    func testLoadPhotos() throws {
+        // Given
+        // Initialize test date and system under test
+        let networkManager = NetworkManager.shared
+        
+        // When
+        // Call system under test
+        let loadPhotosComplete = expectation(description: "photos loaded")
+        
+        networkManager.loadPhotos { response in
+            switch response {
+            case .success(let model):
+                //Then
+                // Verify that output is as expected
+                guard let elementFirst: PhotoElement = model.first else { return }
+                XCTAssertEqual(elementFirst.author, "Alejandro Escamilla")
+                loadPhotosComplete.fulfill()
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+        waitForExpectations(timeout: 8.0, handler: nil)
+    }
+    
+    func testLoadPartPhotos() throws {
+        // Given
+        // Initialize test date and system under test
+        let networkManager = NetworkManager.shared
+        
+        // When
+        // Call system under test
+        let loadPartPhotosComplete = expectation(description: "part photos loaded")
+        
+        networkManager.loadPartPhotos(from: 1) { response in
+            switch response {
+            case .success(let model):
+                //Then
+                // Verify that output is as expected
+                guard let elementFirst: PhotoElement = model.first else { return }
+                XCTAssertEqual(elementFirst.author, "Alejandro Escamilla")
+                loadPartPhotosComplete.fulfill()
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+        waitForExpectations(timeout: 8.0, handler: nil)
+    }
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
